28712254095f31085da8689efec2a66ffe6b15c6
illonion <zhou.lawrence.yz@gmail.com>
2024-07-07 20:49:37 +1000

    Add strain graph

--- showcase/index.js
+++ showcase/index.js
@@ -50,6 +50,21 @@ const countUpAnimation = {
 // IPC State
 let currentState
 
+// Strains
+const progressChart = document.getElementById("progress")
+let tempStrains, seek, fullTime
+let changeStats = false
+let statsCheck = false
+let last_strain_update = 0
+
+window.onload = function () {
+	let ctx = document.getElementById('strain').getContext('2d')
+	window.strainGraph = new Chart(ctx, config)
+
+	let ctxProgress = document.getElementById('strainProgress').getContext('2d')
+	window.strainGraphProgress = new Chart(ctxProgress, configProgress)
+}
+
 socket.onmessage = event => {
     const data = JSON.parse(event.data)
     console.log(data)
@@ -129,4 +144,108 @@ socket.onmessage = event => {
         countUpAnimation.playerDetailsCurrentCombo.update(resultsScreenInfo.maxCombo)
         countUpAnimation.playerDetailsMaxCombo.update(data.menu.bm.stats.maxCombo)
     }
+
+    if (tempStrains != JSON.stringify(data.menu.pp.strains) && window.strainGraph) {
+        tempStrains = JSON.stringify(data.menu.pp.strains)
+        if (data.menu.pp.strains) {
+            let temp_strains = smooth(data.menu.pp.strains, 5)
+			let new_strains = []
+			for (let i = 0; i < 60; i++) {
+				new_strains.push(temp_strains[Math.floor(i * (temp_strains.length / 60))])
+			}
+			new_strains = [0, ...new_strains, 0]
+
+			config.data.datasets[0].data = new_strains
+			config.data.labels = new_strains
+			config.options.scales.y.max = Math.max(...new_strains)
+			configProgress.data.datasets[0].data = new_strains
+			configProgress.data.labels = new_strains
+			configProgress.options.scales.y.max = Math.max(...new_strains)
+			window.strainGraph.update()
+			window.strainGraphProgress.update()
+        } else {
+			config.data.datasets[0].data = []
+			config.data.labels = []
+			configProgress.data.datasets[0].data = []
+			configProgress.data.labels = []
+			window.strainGraph.update()
+			window.strainGraphProgress.update()
+		}
+    }
+
+    let now = Date.now()
+	if (fullTime !== data.menu.bm.time.mp3) { fullTime = data.menu.bm.time.mp3; onepart = 873 / fullTime }
+	if (seek !== data.menu.bm.time.current && fullTime && now - last_strain_update > 300) {
+		last_strain_update = now
+		seek = data.menu.bm.time.current
+
+		if (data.menu.state !== 2) {
+			progressChart.style.maskPosition = '-873px 0px'
+			progressChart.style.webkitMaskPosition = '-873px 0px'
+		}
+		else {
+			let maskPosition = `${-873 + onepart * seek}px 0px`
+			progressChart.style.maskPosition = maskPosition
+			progressChart.style.webkitMaskPosition = maskPosition
+		}
+	}
+}
+
+// Configs are for strain graphs
+let config = {
+	type: 'line',
+	data: {
+		labels: [],
+		datasets: [{
+			borderColor: 'rgba(245, 245, 245, 0)',
+			backgroundColor: 'rgb(109, 111, 115)',
+			data: [],
+			fill: true,
+			stepped: false,
+		}]
+	},
+	options: {
+		tooltips: { enabled: false },
+		legend: { display: false, },
+		elements: { point: { radius: 0 } },
+		responsive: false,
+		scales: {
+			x: { display: false, },
+			y: {
+				display: false,
+				min: 0,
+				max: 100
+			}
+		},
+		animation: { duration: 0 }
+	}
+}
+
+let configProgress = {
+	type: 'line',
+	data: {
+		labels: [],
+		datasets: [{
+			borderColor: 'rgba(245, 245, 245, 0)',
+			backgroundColor: 'rgb(48, 54, 63)',
+			data: [],
+			fill: true,
+			stepped: false,
+		}]
+	},
+	options: {
+		tooltips: { enabled: false },
+		legend: { display: false, },
+		elements: { point: { radius: 0 } },
+		responsive: false,
+		scales: {
+			x: { display: false, },
+			y: {
+				display: false,
+				min: 0,
+				max: 100
+			}
+		},
+		animation: { duration: 0 }
+	}
 }
