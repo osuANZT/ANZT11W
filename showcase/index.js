0cf524fef256894b8fbac3594dc098543c90fe16
illonion <zhou.lawrence.yz@gmail.com>
2024-07-07 19:04:44 +1000

    Roundname working

--- showcase/index.js
+++ showcase/index.js
@@ -8,3 +8,26 @@ async function getMappool() {
 }
 
 getMappool()
+
+// Socket Events
+// Credits: VictimCrasher - https://github.com/VictimCrasher/static/tree/master/WaveTournament
+const socket = new ReconnectingWebSocket("ws://" + location.host + "/ws")
+socket.onopen = () => { console.log("Successfully Connected") }
+socket.onclose = event => { console.log("Socket Closed Connection: ", event); socket.send("Client Closed!") }
+socket.onerror = error => { console.log("Socket Error: ", error) }
+
+// Map Information
+const mapArtistEl = document.getElementById("mapArtist")
+const mapSongAndDifficultyEl = document.getElementById("mapSongAndDifficulty")
+const mapInfoModsTextEl = document.getElementById("mapInfoModsText")
+const mapInfoMapIdEl = document.getElementById("mapInfoMapId")
+// Map Statistics
+const mapStatsCSEl = document.getElementById("mapStatsCS")
+const mapStatsAREl = document.getElementById("mapStatsAR")
+const mapStatsODEl = document.getElementById("mapStatsOD")
+const mapStatsSREl = document.getElementById("mapStatsSR")
+
+socket.onmessage = event => {
+    const data = JSON.parse(event.data)
+    console.log(data)
+}
