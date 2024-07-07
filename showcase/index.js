b577d5e2c2002b4db0acdf1d7d7d05737d655aff
illonion <zhou.lawrence.yz@gmail.com>
2024-07-07 20:02:25 +1000

    Add logic to player score section

--- showcase/index.js
+++ showcase/index.js
@@ -31,12 +31,20 @@ const mapStatsAREl = document.getElementById("mapStatsAR")
 const mapStatsODEl = document.getElementById("mapStatsOD")
 const mapStatsSREl = document.getElementById("mapStatsSR")
 
+// Player details
+const playerNameEl = document.getElementById("playerName")
+const playerScoreEl = document.getElementById("playerScore")
+const playerAccuracyEl = document.getElementById("playerAccuracy")
+const playerDetailsComboEl = document.getElementById("playerDetailsCombo")
+const playerDetailsCurrentComboEl = document.getElementById("playerDetailsCurrentCombo")
+const playerDetailsMaxComboEl = document.getElementById("playerDetailsMaxCombo")
+
 // IPC State
 let currentState
 
 socket.onmessage = event => {
     const data = JSON.parse(event.data)
-    // console.log(data)
+    console.log(data)
 
     if (currentMapId !== data.menu.bm.id || currentMd5 !== data.menu.bm.md5) {
         currentMapId = data.menu.bm.id
@@ -71,5 +79,43 @@ socket.onmessage = event => {
         mapStatsAREl.innerText = data.menu.bm.stats.AR
         mapStatsODEl.innerText = data.menu.bm.stats.OD
         mapStatsSREl.innerText = data.menu.bm.stats.fullSR
+    }
+
+    
+    // Check state
+    if (currentState !== data.menu.state) currentState = data.menu.state
+
+    // Conditionals for stat
+    // This is specifically for the middle section
+    if (currentState === 2) {
+        // Gameplay
+        const gameplayScreenInfo = data.gameplay
+        playerNameEl.innerText = gameplayScreenInfo.name
+        playerScoreEl.innerText = gameplayScreenInfo.score
+        playerAccuracyEl.innerText = `${Math.round(gameplayScreenInfo.accuracy * 100) / 100}%`
+        playerDetailsCurrentComboEl.innerText = gameplayScreenInfo.combo.current
+        playerDetailsMaxComboEl.innerText = data.menu.bm.stats.maxCombo
+        playerDetailsComboEl.style.display = "block"
+    } else if (currentState === 7) {
+        // Results screen
+        const resultsScreenInfo = data.resultsScreen
+
+        let totalAccuracy = resultsScreenInfo[300] * 100 +resultsScreenInfo[100] * 100 / 3 + resultsScreenInfo[50] * 100 / 6
+        let totalSum = Math.round(totalAccuracy / (resultsScreenInfo[300] + resultsScreenInfo[100] + resultsScreenInfo[50] + resultsScreenInfo[0]) * 100) / 100
+        
+        playerNameEl.innerText = resultsScreenInfo.name
+        playerScoreEl.innerText = resultsScreenInfo.score
+        playerAccuracyEl.innerText = `${totalSum}%`
+        playerDetailsCurrentComboEl.innerText = resultsScreenInfo.maxCombo
+        playerDetailsMaxComboEl.innerText = data.menu.bm.stats.maxCombo
+        playerDetailsComboEl.style.display = "block"
+    } else {
+        // Everything else
+        playerNameEl.innerText = ""
+        playerScoreEl.innerText = ""
+        playerAccuracyEl.innerText = ""
+        playerDetailsCurrentComboEl.innerText = ""
+        playerDetailsMaxComboEl.innerText = ""
+        playerDetailsComboEl.style.display = "none"
     }
 }
