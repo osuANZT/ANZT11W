f0d841335ea2d21db527716e1d39905af4e63504
illonion <zhou.lawrence.yz@gmail.com>
2024-07-18 14:55:46 +1000

    Add team name logic

--- mappool/index.js
+++ mappool/index.js
@@ -13,3 +13,34 @@ async function getMappool() {
 }
 
 getMappool()
+
+// Socket Events
+// Credits: VictimCrasher - https://github.com/VictimCrasher/static/tree/master/WaveTournament
+const socket = new ReconnectingWebSocket("ws://" + location.host + "/ws")
+socket.onopen = () => { console.log("Successfully Connected") }
+socket.onclose = event => { console.log("Socket Closed Connection: ", event); socket.send("Client Closed!") }
+socket.onerror = error => { console.log("Socket Error: ", error) }
+
+// Profile Pictures
+const redProfilePictureEl = document.getElementById("redProfilePicture")
+const blueProfilePictureEl = document.getElementById("blueProfilePicture")
+// Player Names
+const redPlayerNameEl = document.getElementById("redPlayerName")
+const bluePlayerNameEl = document.getElementById("bluePlayerName")
+let currentRedPlayerName, currentBluePlayerName
+let currentRedPlayerId, currentBluePlayerId
+
+socket.onmessage = event => {
+    const data = JSON.parse(event.data)
+    console.log(data)
+
+    // Set player names and profile pictures
+    if (currentRedPlayerName !== data.tourney.manager.teamName.left) {
+        currentRedPlayerName = data.tourney.manager.teamName.left
+        redPlayerNameEl.innerText = currentRedPlayerName
+    }
+    if (currentBluePlayerName !== data.tourney.manager.teamName.right) {
+        currentBluePlayerName = data.tourney.manager.teamName.right
+        bluePlayerNameEl.innerText = currentBluePlayerName
+    }
+}
