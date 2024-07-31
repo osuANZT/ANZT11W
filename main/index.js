e2a4642851f2bd9042250390a05492b8581ddb73
illonion <zhou.lawrence.yz@gmail.com>
2024-07-31 21:54:07 +1000

    Add player details logic to main scene

--- main/index.js
+++ main/index.js
@@ -12,11 +12,65 @@ async function getMappool() {
 }
 getMappool()
 
+// Find maps in mappool
+const findMapInMapool = beatmapID => allBeatmaps.find(beatmap => beatmap.beatmapID === beatmapID)
+
 // Get players
 let allPlayers
 async function getPlayers() {
     const response = await fetch("../_data/players.json")
     const responseJson = await response.json()
-    allPlayers = responseJson()
+    allPlayers = responseJson
 }
 getPlayers()
+
+// Get players from player name
+const getPlayersFromName = playerName => allPlayers.find(player => player.playerName === playerName)
+
+// Socket Events
+// Credits: VictimCrasher - https://github.com/VictimCrasher/static/tree/master/WaveTournament
+const socket = new ReconnectingWebSocket("ws://" + location.host + "/ws")
+socket.onopen = () => { console.log("Successfully Connected") }
+socket.onclose = event => { console.log("Socket Closed Connection: ", event); socket.send("Client Closed!") }
+socket.onerror = error => { console.log("Socket Error: ", error) }
+
+// Seeds
+const redPlayerSeedEl = document.getElementById("redPlayerSeed")
+const bluePlayerSeedEl = document.getElementById("bluePlayerSeed")
+// Profile Pictures
+const redProfilePictureEl = document.getElementById("redProfilePicture")
+const blueProfilePictureEl = document.getElementById("blueProfilePicture")
+// Player Names
+const redPlayerNameEl = document.getElementById("redPlayerName")
+const bluePlayerNameEl = document.getElementById("bluePlayerName")
+let currentRedPlayerName, currentBluePlayerName
+let currentRedPlayerId, currentBluePlayerId
+
+socket.onmessage = event => {
+    const data = JSON.parse(event.data)
+    console.log(data)
+
+    // Set player names and profile pictures
+    if (currentRedPlayerName !== data.tourney.manager.teamName.left && allPlayers) {
+        currentRedPlayerName = data.tourney.manager.teamName.left
+        redPlayerNameEl.innerText = currentRedPlayerName
+
+        let playerDetails = getPlayersFromName(currentRedPlayerName)
+        if (playerDetails) {
+            currentRedPlayerId = playerDetails.playerId
+            redPlayerSeedEl.innerText = playerDetails.playerSeed
+            redProfilePictureEl.style.backgroundImage = `url("https://a.ppy.sh/${currentRedPlayerId}")`
+        }
+    }
+    if (currentBluePlayerName !== data.tourney.manager.teamName.right && allPlayers) {
+        currentBluePlayerName = data.tourney.manager.teamName.right
+        bluePlayerNameEl.innerText = currentBluePlayerName
+
+        let playerDetails = getPlayersFromName(currentBluePlayerName)
+        if (playerDetails) {
+            currentBluePlayerId = playerDetails.playerId
+            bluePlayerSeedEl.innerText = playerDetails.playerSeed
+            blueProfilePictureEl.style.backgroundImage = `url("https://a.ppy.sh/${currentBluePlayerId}")`
+        }
+    }
+}
